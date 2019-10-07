2a770bfc25a1584533e7f35cea59265b8fa56d83
J-B Garway-Templeman <jbgarwayt@gmail.com>
2019-10-07 15:04:04 +0100

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  let hours = time.split(':')
+  if (hours[0] < 12) {
+    return "Good Morning"
+  } else if (hours[0] > 17) {
+    return "Good Evening"
+  } else {
+    return "Good Afternoon"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(str) {
+  let mess = document.getElementById('greeting');
+  mess.innerText = str
+}
